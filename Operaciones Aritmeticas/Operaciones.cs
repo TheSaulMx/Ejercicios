c4de0073b2cf2472d8fc23be48787bccd9a7d761
TheSaulMx <62609360+TheSaulMx@users.noreply.github.com>
2022-04-26 17:11:27 -0600

    Agregar archivos de proyecto.

---
+++ Operaciones Aritmeticas/Operaciones.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OperacionesAritmeticas
+{
+    /// <summary>
+    /// Autor:SaulMx
+    /// Descripcion:Esta clase realiza cuatro operacions aritmeticas + - * /.
+    /// Fecha: 20/04/22
+    /// </summary>
+    public class OperacionesAritmeticas
+    {
+        int num1;
+        int num2;
+        int resSuma;
+        int resResta;
+        int resMult;
+        int resDiv;
+
+        //Constructor. Debe ir vacío para permitir que se ejecute el código.
+        public OperacionesAritmeticas()
+        {
+            
+        }
+
+        //Metodo Suma
+        public void OperacionSuma()
+        {
+            num1 = 8;
+            num2 = 2;
+            resSuma = num1 + num2;
+            Console.WriteLine("El resultado es: " + resSuma);
+        }
+        
+        //Metodo Resta
+        public void OperacionResta()
+        {
+            num1 = 8;
+            num2 = 2;
+            resResta = num1 - num2;
+            Console.WriteLine("El resultado es: " + resResta);
+        }
+        
+        //Metodo Multiplicacion
+        public void OperacionMultiplicacion()
+        {
+            num1 = 8;
+            num2 = 2;
+            resMult = num1 * num2;
+            Console.WriteLine("El resultado es: " + resMult);
+        }
+        
+        //Metodo Suma
+        public void OperacionDiv()
+        {
+            num1 = 8;
+            num2 = 2;
+            resDiv = num1 + num2;
+            Console.WriteLine("El resultado es: " + resDiv);
+        }
+
+
+    }
+}
---
+++ Operaciones Aritmeticas/OperacionesConParametros.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones_Aritmeticas_Con_Parametros
+{
+    /// <summary>
+    /// Autor:SaulMx
+    /// Descripcion:Esta clase realiza cuatro operacions aritmeticas + - * / utilizando parametros y return.
+    /// Fecha: 20/04/22
+    /// </summary>
+    public class OperacionesConParametros
+    {
+
+        public OperacionesConParametros()
+        {
+
+        }
+        public void nombre()
+        {
+
+        }
+    }
+}
