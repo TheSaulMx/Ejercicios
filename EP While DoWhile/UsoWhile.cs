4374777b516fce2ed38835bcb423645b0913aeca
TheSaulMx <62609360+TheSaulMx@users.noreply.github.com>
2022-05-02 18:33:30 -0600

    Uso de while y doWhile

---
+++ EP While DoWhile/UsoWhile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP_While_DoWhile
+{
+    public class UsoWhile
+    {
+        public int c { get; set; }
+
+        public UsoWhile()
+        {
+        }
+
+        public void ContadorMas()
+        {
+            int c = 1;
+
+            while (c <= 20)
+            {
+                Console.WriteLine(c);
+                c++;
+                System.Threading.Thread.Sleep(500);
+            }
+            Console.WriteLine("==");
+
+        }  
+
+        public void ContadorMenos()
+        {
+            int c = 20;
+
+            do
+            {
+                Console.WriteLine(c);
+                c--;
+                System.Threading.Thread.Sleep(500);
+            } while (c > 0);
+
+        }
+
+    }
+
+
+
+}
