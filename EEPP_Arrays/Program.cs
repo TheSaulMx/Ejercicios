c27a0f6da6899971b2fe7caea1c54bbb8857972e
TheSaulMx <62609360+TheSaulMx@users.noreply.github.com>
2022-05-05 19:29:28 -0600

    Fixed

--- EEPP_Arrays/Program.cs
+++ EEPP_Arrays/Program.cs
@@ -2,5 +2,17 @@
 
 Boletas boletafinal = new Boletas();
 
-boletafinal.Boleta();
+//boletafinal.Boleta();
+
+//1 En un arreglo de 10 alumnos vas a pedir los nombre de cada uno de ellos usando un metodo recursivo y al final los vas mostrar 
+
+EP1 alumnos = new EP1();
+alumnos.ArrayAlumnos();
+
+//2 En un método bidimensional vas a pedir el nombre y edades de cada uno de los alumnos y los vas a mostrar (5 alumnos)
+
+EP2 nombreEdades = new EP2();
+//nombreEdades.Nombre_Edades();
+
+
 
